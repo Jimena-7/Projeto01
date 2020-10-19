e096f91b5731d74ef585b1aad25ca17f47880018
Jimena-7 <jimejardi07@gmail.com>
2020-10-19 17:18:37 -0300

    Commit 2

--- app/src/main/java/br/senai/sc/myapplication/MainActivity.java
+++ app/src/main/java/br/senai/sc/myapplication/MainActivity.java
@@ -1,14 +1,113 @@
 package br.senai.sc.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import br.senai.sc.myapplication.modelo.Produto;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_CODE_NOVO_PRODUTO = 1;
+    private final int RESULT_CODE_NOVO_PRODUTO = 10;
+    private final int REQUEST_CODE_EDITAR_PRODUTO = 2;
+    private final int RESULT_CODE_PRODUTO_EDITADO = 11;
+
+
+    private ListView ListViewProdutos;
+    private ArrayAdapter<Produto> adapterProdutos;
+    private int id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Produtos");
+
+        ListViewProdutos = findViewById(R.id.listView_produtos);
+        ArrayList<Produto>produtos = new ArrayList<Produto>();
+
+        adapterProdutos = new ArrayAdapter<Produto>(MainActivity.this, android.R.layout.simple_list_item_1, produtos);
+        ListViewProdutos.setAdapter(adapterProdutos);
+
+        definirOnClickListenerListView();
+
+        //Menu deslizante
+        TextView textView = findViewById(R.id.textView);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_deslizante, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_1:
+                Toast.makeText(this, "Opção desativada", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.option_2:
+                Toast.makeText(this, "Deseja excluir o item?", Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
+    private void definirOnClickListenerListView() {
+        ListViewProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapterProdutos.getItem(position);
+                Produto produtoClicado = adapterProdutos.getItem(position);
+                Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+                intent.putExtra("produtoEdicao", produtoClicado);
+                startActivityForResult(intent, REQUEST_CODE_EDITAR_PRODUTO);
+
+            }
+        });
+    }
+
+
+    public void onClickNovoProduto(View v) {
+        Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_NOVO_PRODUTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_NOVO_PRODUTO && resultCode == RESULT_CODE_NOVO_PRODUTO) {
+            Produto produto = (Produto) data.getExtras().getSerializable("novoProduto");
+            id = id + 1;
+            produto.setId(id);
+            this.adapterProdutos.add(produto);
+        }else if (requestCode == REQUEST_CODE_EDITAR_PRODUTO && resultCode == RESULT_CODE_PRODUTO_EDITADO) {
+            Produto produtoEditado = (Produto) data.getExtras().getSerializable("produtoEditado");
+            for (int i = 0; 1 < adapterProdutos.getCount(); i++){
+                Produto produto = adapterProdutos.getItem(i);
+                if (produto.getId() == produtoEditado.getId()){
+                    adapterProdutos.remove(produto);
+                    adapterProdutos.insert(produtoEditado, i);
+                    break;
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
